bbe175db1eedeea9bd3220934bbdff7a93b9c7b8
hank_mac <1208481203@qq.com>
2025-03-12 22:53:55 +1100

    delete some unused function in 2025

--- django_web_app/blog/urls.py
+++ django_web_app/blog/urls.py
@@ -1,48 +1,46 @@
 from django.urls import path
 from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-    UserPostListView,
-    GPTAudioCreateView, GPTAudioUpdateView, ImageCreateView, GPTChatCreateView, GPTImageView, SaveChat, LoadChat,
+    PostListView, ImageCreateView, GPTChatCreateView, GPTImageView, SaveChat, LoadChat,
     DeleteChat, GPT4ImageView, DeleteGPT4Image, ppt2speech, pptSave, pptPlay, a1_view,
 )
 from . import views
 
 
-
+# URL 对应处理
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),  # 首页
 
-    path('post/gptchat/', GPTChatCreateView.as_view(), name='gpt-chat'),  # gpt聊天界面
+    # gpt基础聊天
+    path('post/gptchat/', GPTChatCreateView.as_view(), name='gpt-chat'),
     path('post/savechat/', SaveChat.as_view(), name='save-chat'),
     path('post/loadchat/', LoadChat.as_view(), name='load-chat'),
     path('post/deletechat/', DeleteChat.as_view(), name='delete-chat'),
+    # GPT图片处理
     path('post/gptchat/image', GPTImageView.as_view(), name='gpt-image'),
     path('post/gpt4/image', GPT4ImageView.as_view(), name='gpt4-image'),
     path('post/gpt4/image/delete', DeleteGPT4Image.as_view(), name='gpt4-image-delete'),
-
+    # PPT演讲
     path('post/ppt2speech/', ppt2speech.as_view(), name='ppt-speech'),
     path('post/ppt2speech/save', pptSave.as_view(), name='ppt-speech-save'),
     path('post/ppt2speech/play', pptPlay.as_view(), name='ppt-speech-play'),
     path('download_speech/<int:slide_index>/', views.download_speech, name='download_speech'),
-
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/gptnew/', GPTAudioCreateView.as_view(), name='gpt-audio'),
-    path('update/gptnew/', GPTAudioUpdateView.as_view(), name='gpt-audio-update'),
-
+    # GPT语音版 已过时
+    # path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # path('post/gptnew/', GPTAudioCreateView.as_view(), name='gpt-audio'),
+    # path('update/gptnew/', GPTAudioUpdateView.as_view(), name='gpt-audio-update'),
+    # 生成图片
     path('image/new/', ImageCreateView.as_view(), name='image-generate'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    # 朋友圈文案
+    # path('post/new/', PostCreateView.as_view(), name='post-create'),
+    # path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    #
+    # path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    # path('media/Files/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
 
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('media/Files/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
     path('search/', views.search, name='search'),
     path('about/', views.about, name='blog-about'),
 
+    # 检查用户登陆状态
     path('check-user-status/', views.check_user_logged_in, name='check_user_status'),
 
     path('a1/', a1_view, name='my_view_a1'),
--- django_web_app/blog/views.py
+++ django_web_app/blog/views.py
@@ -1,3 +1,7 @@
+"""
+处理前端所有传过来的请求 根据template名称
+"""
+
 import base64
 import json
 import re
@@ -85,25 +89,25 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
 
-
-class UserPostListView(ListView):
-    model = Post
-    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'posts'
-    paginate_by = 4  # 一页显示4条
-
-    def get_queryset(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))
-        #  prefetch_related('photo_category')  =》 through='Post_Photo_Category'
-        #  使用prefetch_related方法是为了优化数据库查询。当你在之后访问与Post对象关联的photo_category多对多关系时，
-        #  它将减少数据库查询的数量。在这里，与每个Post对象相关的photo_category将被预先加载，
-        #  这样当您循环遍历每个Post对象并访问其photo_category时，不需要为每个Post对象进行额外的数据库查询。
-        return Post.objects.filter(author=user).order_by('-date_posted').prefetch_related('photo_category')
-
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
+# 发布记录
+# class UserPostListView(ListView):
+#     model = Post
+#     template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+#     context_object_name = 'posts'
+#     paginate_by = 4  # 一页显示4条
+#
+#     def get_queryset(self):
+#         user = get_object_or_404(User, username=self.kwargs.get('username'))
+#         #  prefetch_related('photo_category')  =》 through='Post_Photo_Category'
+#         #  使用prefetch_related方法是为了优化数据库查询。当你在之后访问与Post对象关联的photo_category多对多关系时，
+#         #  它将减少数据库查询的数量。在这里，与每个Post对象相关的photo_category将被预先加载，
+#         #  这样当您循环遍历每个Post对象并访问其photo_category时，不需要为每个Post对象进行额外的数据库查询。
+#         return Post.objects.filter(author=user).order_by('-date_posted').prefetch_related('photo_category')
+#
+#
+# class PostDetailView(DetailView):
+#     model = Post
+#     template_name = 'blog/post_detail.html'
 
 
 class ppt2speech(CreateView):
@@ -230,174 +234,174 @@ class PostForm(forms.ModelForm):  # 定义的表单
     photo_category = forms.ModelMultipleChoiceField(
         queryset=Category.objects.all(),
         widget=forms.SelectMultiple(attrs={'size': '10'}))
+#
+# # 朋友圈文本生成
+# class PostCreateView(LoginRequiredMixin, CreateView):
+#     template_name = 'blog/post_form.html'
+#     # fields = ['file', 'language', 'platform', 'photo_category', 'special_request']
+#     form_class = PostForm  # 使用我们定义的表单
+#
+#     def form_valid(self, form):  # 后端处理
+#         form.instance.author = self.request.user
+#
+#         # First save the form to ensure that the file is saved
+#         # ['file', 'language', 'platform', 'photo_category', 'special_request'] 已经到数据库中了！
+#         response = super().form_valid(form)
+#
+#         # # Notify user that the processing is starting
+#         # messages.info(self.request, "Processing started. Please wait...  文本生成中")
+#
+#         # Now, the file should be saved and we can get its path
+#         relative_path = self.object.file.name
+#         full_path = os.path.join(settings.MEDIA_ROOT, relative_path)
+#
+#         other_data = {
+#             'language': form.cleaned_data['language'],
+#             'platform': form.cleaned_data['platform'],
+#             'photo_category': form.cleaned_data['photo_category'],
+#             'special_request': form.cleaned_data['special_request'],
+#         }
+#         # Check for empty values
+#         if not full_path or not other_data:
+#             messages.error(self.request, "The file path or other data is missing. Please try again.")
+#             return response
+#
+#         generated_text = generate_text(full_path, other_data)
+#         self.object.generate_text = generated_text
+#         self.object.save()  # Save the updated data
+#
+#         # Notify user that the processing is done
+#         messages.success(self.request, "Processing completed successfully! 生成成功！")
+#
+#         return response
 
+# 朋友圈文案更正
+# class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+#     model = Post
+#     template_name = 'blog/post_form.html'
+#
+#     def post(self, request, *args, **kwargs):
+#         post_id = self.kwargs['pk']
+#         post = get_object_or_404(Post, id=post_id)  # 数据库获取信息
+#         new_content = request.POST.get("content")
+#
+#         if new_content:
+#             post.generate_text = new_content
+#             post.save()
+#
+#         return HttpResponseRedirect(self.get_success_url())
+#
+#     def test_func(self):
+#         post = self.get_object()
+#         if self.request.user == post.author:
+#             return True
+#         return False
+#
+#     def get_success_url(self):
+#         return reverse('user-posts', kwargs={'username': self.request.user.username})
 
-class PostCreateView(LoginRequiredMixin, CreateView):  # 朋友圈文本生成
-    template_name = 'blog/post_form.html'
-    # fields = ['file', 'language', 'platform', 'photo_category', 'special_request']
-    form_class = PostForm  # 使用我们定义的表单
-
-    def form_valid(self, form):  # 后端处理
-        form.instance.author = self.request.user
-
-        # First save the form to ensure that the file is saved
-        # ['file', 'language', 'platform', 'photo_category', 'special_request'] 已经到数据库中了！
-        response = super().form_valid(form)
-
-        # # Notify user that the processing is starting
-        # messages.info(self.request, "Processing started. Please wait...  文本生成中")
-
-        # Now, the file should be saved and we can get its path
-        relative_path = self.object.file.name
-        full_path = os.path.join(settings.MEDIA_ROOT, relative_path)
-
-        other_data = {
-            'language': form.cleaned_data['language'],
-            'platform': form.cleaned_data['platform'],
-            'photo_category': form.cleaned_data['photo_category'],
-            'special_request': form.cleaned_data['special_request'],
-        }
-        # Check for empty values
-        if not full_path or not other_data:
-            messages.error(self.request, "The file path or other data is missing. Please try again.")
-            return response
-
-        generated_text = generate_text(full_path, other_data)
-        self.object.generate_text = generated_text
-        self.object.save()  # Save the updated data
-
-        # Notify user that the processing is done
-        messages.success(self.request, "Processing completed successfully! 生成成功！")
-
-        return response
-
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    template_name = 'blog/post_form.html'
-
-    def post(self, request, *args, **kwargs):
-        post_id = self.kwargs['pk']
-        post = get_object_or_404(Post, id=post_id)  # 数据库获取信息
-        new_content = request.POST.get("content")
-
-        if new_content:
-            post.generate_text = new_content
-            post.save()
-
-        return HttpResponseRedirect(self.get_success_url())
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
-    def get_success_url(self):
-        return reverse('user-posts', kwargs={'username': self.request.user.username})
-
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = '/'
-    template_name = 'blog/post_confirm_delete.html'
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
+# 朋友圈文案删除
+# class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+#     model = Post
+#     success_url = '/'
+#     template_name = 'blog/post_confirm_delete.html'
+#
+#     def test_func(self):
+#         post = self.get_object()
+#         if self.request.user == post.author:
+#             return True
+#         return False
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
-
-class GPTAudioCreateView(LoginRequiredMixin, CreateView):
-    model = PostAudio
-    template_name = 'blog/gpt_audio.html'
-    fields = ['request', 'generate_text', 'chat_id']
-
-    @method_decorator(csrf_exempt)  # 确保这个view可以被上面的JS代码POST
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-
-    # 处理提交的文件
-    def post(self, request, *args, **kwargs):
-        audio_file = request.FILES.get('audio')
-        if audio_file:
-            chat_id = request.POST.get('chat_id')
-            # 获取上下文
-            records = PostAudio.objects.filter(author=self.request.user, chat_id=chat_id).order_by('-date_posted')
-            combined_request = ""
-            if records:
-                # 遍历查询集并从每个对象中获取request_data
-                data_strings = [req_record.request for req_record in records]
-                # 将所有的数据连接成一个字符串
-                combined_request = ' '.join(data_strings)
-            # print(combined_request)
-
-            response_data = gpt_audio_response(audio_file, self.request.user, combined_request)
-            user_transcript = clean_text(response_data.get('user_transcript'))
-            gpt_response = clean_text(response_data.get('gpt_response'))
-
-            # 创建新的PostAudio对象并保存到数据库
-            post_audio = PostAudio(author=self.request.user, request=user_transcript, generate_text=gpt_response,
-                                   chat_id=chat_id)
-            post_audio.save()
-
-            # 如果想要在响应中返回更多数据，您可以在此处修改
-            return JsonResponse(response_data, status=200)  # user_transcript, gpt_response
-
-        return JsonResponse({'message': 'No audio received.'}, status=400)
-
-
-class GPTAudioUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = PostAudio
-
-    @method_decorator(csrf_exempt)  # 确保这个view可以被上面的JS代码POST
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        # template_name = 'blog/post_form.html'
-        # fields = ['request', 'generate_text', 'chat_id']  # 需要更新的字段
-        try:
-            data = json.loads(request.body)
-            edited_text = data.get('text')
-            chat_id = data.get('chat_id')
-
-            # 寻找相同chat_id并检索最新的date_posted数据
-            record = PostAudio.objects.filter(chat_id=chat_id).order_by('-date_posted').first()
-
-            if record:
-                # 更新并保存记录
-                # 查询上下文
-                records = PostAudio.objects.filter(author=self.request.user, chat_id=chat_id).order_by('-date_posted')
-                combined_request = ""
-                if records:
-                    # 遍历查询集并从每个对象中获取request_data
-                    data_strings = [req_record.request for req_record in records]
-                    # 将所有的数据连接成一个字符串
-                    combined_request = ' '.join(data_strings)
-                print(combined_request)
-
-                record.request = clean_text(edited_text)
-                record.generate_text = clean_text(gpt_text_response(edited_text, combined_request).get('gpt_response'))
-                record.date_posted = timezone.now()
-                record.save()
-                # print(gpt_text_response(edited_text).get('gpt_response'))
-                # 返回成功信息和更新后的文本
-                return JsonResponse(gpt_text_response(edited_text, combined_request), status=200)
-
-            else:
-                # 如果没有找到匹配的记录
-                return JsonResponse({"success": False, "error": "Record not found"})
-
-        except ObjectDoesNotExist:
-            return JsonResponse({"success": False, "error": "Record not found"})
-        except Exception as e:
-            return JsonResponse({"success": False, "error": str(e)})
+# GPT语音版
+# class GPTAudioCreateView(LoginRequiredMixin, CreateView):
+#     model = PostAudio
+#     template_name = 'blog/gpt_audio.html'
+#     fields = ['request', 'generate_text', 'chat_id']
+#
+#     @method_decorator(csrf_exempt)  # 确保这个view可以被上面的JS代码POST
+#     def dispatch(self, *args, **kwargs):
+#         return super().dispatch(*args, **kwargs)
+#
+#     # 处理提交的文件
+#     def post(self, request, *args, **kwargs):
+#         audio_file = request.FILES.get('audio')
+#         if audio_file:
+#             chat_id = request.POST.get('chat_id')
+#             # 获取上下文
+#             records = PostAudio.objects.filter(author=self.request.user, chat_id=chat_id).order_by('-date_posted')
+#             combined_request = ""
+#             if records:
+#                 # 遍历查询集并从每个对象中获取request_data
+#                 data_strings = [req_record.request for req_record in records]
+#                 # 将所有的数据连接成一个字符串
+#                 combined_request = ' '.join(data_strings)
+#             # print(combined_request)
+#
+#             response_data = gpt_audio_response(audio_file, self.request.user, combined_request)
+#             user_transcript = clean_text(response_data.get('user_transcript'))
+#             gpt_response = clean_text(response_data.get('gpt_response'))
+#
+#             # 创建新的PostAudio对象并保存到数据库
+#             post_audio = PostAudio(author=self.request.user, request=user_transcript, generate_text=gpt_response,
+#                                    chat_id=chat_id)
+#             post_audio.save()
+#
+#             # 如果想要在响应中返回更多数据，您可以在此处修改
+#             return JsonResponse(response_data, status=200)  # user_transcript, gpt_response
+#
+#         return JsonResponse({'message': 'No audio received.'}, status=400)
+#
+#
+# class GPTAudioUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+#     model = PostAudio
+#
+#     @method_decorator(csrf_exempt)  # 确保这个view可以被上面的JS代码POST
+#     def dispatch(self, *args, **kwargs):
+#         return super().dispatch(*args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         # template_name = 'blog/post_form.html'
+#         # fields = ['request', 'generate_text', 'chat_id']  # 需要更新的字段
+#         try:
+#             data = json.loads(request.body)
+#             edited_text = data.get('text')
+#             chat_id = data.get('chat_id')
+#
+#             # 寻找相同chat_id并检索最新的date_posted数据
+#             record = PostAudio.objects.filter(chat_id=chat_id).order_by('-date_posted').first()
+#
+#             if record:
+#                 # 更新并保存记录
+#                 # 查询上下文
+#                 records = PostAudio.objects.filter(author=self.request.user, chat_id=chat_id).order_by('-date_posted')
+#                 combined_request = ""
+#                 if records:
+#                     # 遍历查询集并从每个对象中获取request_data
+#                     data_strings = [req_record.request for req_record in records]
+#                     # 将所有的数据连接成一个字符串
+#                     combined_request = ' '.join(data_strings)
+#                 print(combined_request)
+#
+#                 record.request = clean_text(edited_text)
+#                 record.generate_text = clean_text(gpt_text_response(edited_text, combined_request).get('gpt_response'))
+#                 record.date_posted = timezone.now()
+#                 record.save()
+#                 # print(gpt_text_response(edited_text).get('gpt_response'))
+#                 # 返回成功信息和更新后的文本
+#                 return JsonResponse(gpt_text_response(edited_text, combined_request), status=200)
+#
+#             else:
+#                 # 如果没有找到匹配的记录
+#                 return JsonResponse({"success": False, "error": "Record not found"})
+#
+#         except ObjectDoesNotExist:
+#             return JsonResponse({"success": False, "error": "Record not found"})
+#         except Exception as e:
+#             return JsonResponse({"success": False, "error": str(e)})
 
     def test_func(self):
         return True
